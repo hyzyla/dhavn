d00eb3e4da78cb1fc4745266688ff2cdc60704fd
Yevhenii Hyzyla <y.hyzyla@smartweb.com.ua>
2020-03-22 19:13:13 +0200

    Add manual instrunction

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 ## dhavn
+
+1) Create droplet/vm/etc
+2) (REMOTE) Install docker and sshfs
+3) Connect with port forwarding
+```shell script
+ssh root@68.183.208.150 -R 10000:yhyzyla:22 -L 5000:localhost:5000
+```
+
+4) Connect to local directory (ssh server on local machine is required)
+```shell script
+sshfs -p 10000 -o idmap=user,nonempty y.hyzyla@localhost:/home/y.hyzyla/Work/dhavn workdir
+```
+
+5) Open localhost:5000 in browser
--- main.py
+++ main.py
@@ -4,14 +4,8 @@ from compose.network import Network
 from pprint import pprint
 
 project = get_project(project_dir='example')
+web: Service = project.get_service(name='web')
+print(web)
+
 redis: Service = project.get_service(name='redis')
-# pprint(redis.__dict__)
-new_redis = Service(
-    name='new_redis',
-    client=redis.client,
-    default_platform=redis.default_platform,
-    project=redis.project,
-    **redis.options,
-)
-project.services.append(new_redis)
-print(project.create('new_redis'))
+print(redis.options)
